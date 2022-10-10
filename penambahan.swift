08a8b53bd9cb7a4cc9150bcd50ab2795bc5dac1e
fauziardha1 <fauziardasaputra@gmail.com>
2022-10-10 16:56:09 +0700

    resolve conflict

--- penambahan.swift
+++ penambahan.swift
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
 print("under maintenance")
+=======
+func addition(_ a: Int, _ b: Int) -> Int {
+    print("feature berajalan baik baik saja")
+    return a + b
+}
+>>>>>>> dd067f3 (add label)
